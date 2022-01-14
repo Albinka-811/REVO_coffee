cdb49e7ba8a86bd426fefa984293688ac28fcdf8
Albina Holovach <albinkasolodenko@gmail.com>
2022-01-14 19:50:31 +0200

    create plugin for slide

---
+++ src/plugins/albinaCarousel.js
@@ -0,0 +1,67 @@
+console.log('Slider module')
+function AlyaSwiper () {
+    let currentSlideIndex = 0;
+    let slides;
+    let controls = {
+        nextBtn: null,
+    }
+
+    function prevSlide () {
+        let targetIndex;
+
+        // Сделать проверку на индекс меньше 0
+        if (currentSlideIndex - 1 < 0) {
+            targetIndex = slides.length - 1;
+        } else {
+            targetIndex = currentSlideIndex - 1;
+        }
+
+        changeSlide(targetIndex);
+    }
+
+    function nextSlide () {
+        let targetIndex;
+        // Сделать проверку на выход из длины массива (количества элементов в слайдере)
+        if (currentSlideIndex + 1 >= slides.length) {
+            targetIndex = 0;
+        } else {
+            targetIndex = currentSlideIndex + 1;
+        }
+
+        changeSlide(targetIndex);
+    }
+
+    function changeSlide (targetIndex) {
+        hideSlide(slides[currentSlideIndex]);
+        currentSlideIndex = targetIndex;
+        showSlide(slides[currentSlideIndex]);
+    }
+
+    function showSlide (slide) {
+        slide.classList.add('active');
+    }
+
+    function hideSlide (slide) {
+        slide.classList.remove('active');
+    }
+
+    function init ({ slideSelector, rightButtonSelector }) {
+        slides = document.querySelectorAll(slideSelector);
+
+        controls.nextBtn = document.querySelector(rightButtonSelector);
+
+        controls.nextBtn.addEventListener('click', nextSlide);
+
+        for (let i = 1; i < slides.length; i++) {
+            hideSlide(slides[i]);
+        }
+    }
+
+    return {
+        init
+    };
+}
+
+export {
+    AlyaSwiper
+};
