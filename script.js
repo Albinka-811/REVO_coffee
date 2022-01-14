70c69e072a002bd6f340c919ac701fe09579e8a7
Albina Holovach <albinkasolodenko@gmail.com>
2022-01-14 20:26:38 +0200

    Init slider script for combo slides

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-import { AlyaSwiper } from './albinaCarousel'
+import { AlyaSwiper } from './src/plugins/albinaCarousel'
 
 const aboutSlider = new AlyaSwiper()
 aboutSlider.init({
     slideSelector: '.slider-products',
     rightButtonSelector: '#nextSlideBtn',
+})
+
+const secondSlider = new AlyaSwiper()
+secondSlider.init({
+    slideSelector: '.combo-products',
+    rightButtonSelector: '#nextSlideBtnC',
 })
