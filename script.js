7bcd844502ea84f67d74901cf169c8d8b73911ab
Albina Holovach <albinkasolodenko@gmail.com>
2022-01-14 19:52:15 +0200

    connect slider plugin

---
+++ script.js
@@ -0,0 +1,7 @@
+import { AlyaSwiper } from './albinaCarousel'
+
+const aboutSlider = new AlyaSwiper()
+aboutSlider.init({
+    slideSelector: '.slider-products',
+    rightButtonSelector: '#nextSlideBtn',
+})
